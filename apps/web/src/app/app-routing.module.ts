3081c7b4813231b096f7d72cc42706e0807a3b81
Vadym Parakonnyi <v.parakonnyi@gmail.com>
2020-08-04 18:22:56 +0300

    fix endpoint ss

--- apps/web/src/app/app-routing.module.ts
+++ apps/web/src/app/app-routing.module.ts
@@ -19,7 +19,6 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes, {
       preloadingStrategy: PreloadAllModules,
-      useHash: true,
     }),
   ],
   exports: [RouterModule],
--- libs/web/core/src/lib/environments/environment.prod.ts
+++ libs/web/core/src/lib/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  wsEndpoint: 'ws://nx-test-chat.herokuapp.com:8081',
+  wsEndpoint: 'wss://nx-test-chat.herokuapp.com:8081',
   reconnectInterval: 2000,
   reconnectAttempts: 3,
 };
